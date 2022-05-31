982ea848a2f549458482c56ade74bfc175fcbfe5
okarim02 <91153222+okarim02@users.noreply.github.com>
2022-05-31 17:13:37 +0200

    Test fuzzy green 1

--- fuzzyLogic.js
+++ fuzzyLogic.js
@@ -1,107 +1,93 @@
 const fuzzylogic = require('fuzzylogic');
-const common = require('./common');
+// version JS
+var FuzzyLogic=function(){"use strict";var a=function(){};return a.prototype={getResult:function(a){var b=this.construct(a.variables_input),c=this.fuzzification(a.crisp_input,b),d=this.output_combination(c,a.inferences,a.variable_output),e=this.takeMaxOfArraySet(d),f=this.defuzzification(e,this.construct_variable(a.variable_output.sets));return f},construct:function(a){var c,b=[];for(c=a.length-1;c>=0;c-=1)b[c]=this.construct_variable(a[c].sets);return b},construct_variable:function(a){var c,b=[];for(c=a.length-1;c>=0;c-=1)b[c]={a:a[c],firstPoint:a[c][0]===a[c][1]?1:0,lastPoint:a[c][2]===a[c][3]?1:0,mUp:1/(a[c][1]-a[c][0]),mDown:1/(a[c][3]-a[c][2])};return b},fuzzification:function(a,b){var d,c=[];for(d=b.length-1;d>=0;d-=1)c[d]=this.fuzzification_variable(a[d],b[d]);return c},fuzzification_variable:function(a,b){var d,c=[];for(d=b.length-1;d>=0;d-=1)c[d]=this.fuzzification_function(a,b[d]);return c},fuzzification_function:function(a,b){var c=0;return b.a[0]>=a?c=b.firstPoint:b.a[1]>a?c=b.mUp*(a-b.a[0]):b.a[2]>=a?c=1:b.a[3]>a?c=1-b.mDown*(a-b.a[2]):a>=b.a[3]&&(c=b.lastPoint),c},output_combination:function(a,b,c){var e,f,d=[];for(e=c.sets.length-1;e>=0;e-=1)d[e]=[];for(e=b.length-1;e>=0;e-=1)for(f=b[e].length-1;f>=0;f-=1)b[e][f]>=0&&d[b[e][f]].push(a[e][f]);return d},defuzzification:function(a,b){var e,f,g,h,i,j,k,l,m,n,o,p,q,r,c=0,d=0;for(e=a.length-1;e>=0;e-=1)f=b[e],g=f.a,h=a[e],i=g[3]-g[0],k=g[0],g[0]!==g[1]&&(k+=h/f.mUp),l=g[3],g[2]!==g[3]&&(l-=h/f.mDown),m=0,g[0]!==k&&(m+=(k-g[0])*a[e]/2),k!==l&&(m+=(l-k)*a[e]),l!==g[3]&&(m+=(g[3]-l)*a[e]/2),j=l-k,n=h/3*(i+2*j)/(j+i),p=k+(l-k)/2,q=g[0]+(g[3]-g[0])/2,r=0,0!==p-q&&(r=h/(p-q)),o=q,0!==r&&(o+=n/r),c+=m*o,d+=m;return 0===d?0:c/d},takeMaxOfArraySet:function(a){var c,b=[];for(c=a.length-1;c>=0;c-=1)b[c]=this.takeMaxOfArray(a[c]);return b},takeMaxOfArray:function(a){var c,b=a[0];for(c=1;a.length>c;c+=1)b=a[c]>b?a[c]:b;return b}},a}();
+var rules = require('./fuzzy_logic-rules');
 
-/*
-    Linguistic terms for now : excellant, medium, bad. 
+/**
+ * Sr.No
+ * 1) 
+ * IF PageSize=Excellent AND RequestNb=Excellent AND DOMsize=Excellent then Sustainability is Excellent
+ * IF Size=MEDIUM AND RequestNb= Medium AND DOMsize= Medium then Green
+ * IF Size=MEDIUM AND RequestNb= Medium AND DOMsize= Bad then Not Green
 */
 
-function average(array){
-    let sum = 0 ;
-    for(let i = 0 ; i < array.length;i++){ 
-        sum+=array[i];
-    }
-    return sum/array.length;
-}
-
-function getMinMax(array){
-    return [Math.min(...array),Math.max(...array)];
-}
-
-function getEcart(values,moyenne){
-    let sum = 0 ;
-    for(let i of values){
-        sum+= Math.pow((i-moyenne),2);
-    }
-    return Math.sqrt(sum/values.length);
-}
-
-function getSpecificData(data,critere){
- 
-    let valuesData = []
-    data.map((obj)=>{
-        Object.values(obj).forEach(e=>{
-            valuesData.push(e[critere])
-        })
-    })
-    valuesData = valuesData.filter((el)=>{
-        return !isNaN(parseInt(el)) && isFinite(el) && el!=0; // delete 0 and undefined values.
-    })
-
-    console.log(critere+" DATA : ",valuesData);
-
-    if(valuesData.length==0) return ;
-
-    let minMax = getMinMax(valuesData);
-
-    console.log("Min, Max : ",minMax[0],minMax[1]);
-
-    let moyenne = average(valuesData);
-
-    console.log(`Moyenne ${critere} data : `, moyenne);
-
-    // todo : Prendre l'écart type ? 
-    //let ecart = minMax[1] - moyenne;
-    let ecart = getEcart(valuesData,moyenne);
-
-    console.log("Ecart choisit : ",ecart);
-
-    return {
-        "values":valuesData,
-        "min": minMax[0],
-        "max":minMax[1],
-        "average":moyenne,
-        "ecart":ecart,
+function main(){
+    data = {
+        min : 0,
+        aver : 20,
+        ecar : 5,
+        max : 50
     }
 
+    let fuzzyvalue = getFuzzyValue(10,data);
+
+    // WTF ?
+    var resGrade = fuzzylogic.grade(3,0,1);
+    console.log(resGrade)
+
+    let real_fuzzyvalue = rules.and(fuzzyvalue_pageSize, requestNb, (),())
+
 }
 
-module.exports.launch = async function launch(data,url_data=common.exampleScrapperData){
-    // test critères : 
-    let crit_less = ["size","nbRequest","domSize","cssOrJsNotExt","ratioimagesResizedInPage","ratioHttp1","fileNotMin","filesWithError","isStatic","socialButtonFound","nbOfImagesWithSrcEmpty","loadTime","ratioLazyLoad","cssFiles"];
-    // CMS ? => si aucun cms : good , sinon medium ou bad si bcp de plugins 
-    let crit_more = ["etagsNb"];
-    var fuzzification = {};
-    // Test
-    console.log(`Fuzzy logic`);
-    for(let i of crit_less){
-        console.log("Critère testé :",i);
-        const result = getSpecificData(data,i);
-        
-        if(!result) continue; 
-        
-        if(result.min > url_data[i]){
-            result.min = url_data[i];
-        }
-        if(result.max < url_data[i]){
-            result.max = url_data[i];
-        }
+function fuzzyJS(){
+    
+    var obj = {
+		crisp_input: [150, 10, 10],
+		variables_input: [
+			{
+				name: "Size",
+				setsName: ["Excellent", "Medium", "Bad"],
+				sets: [
+					[0,0,25,150],
+					[25,150,150,300],
+					[150,300,400,400]
+				]
+			},
+			{
+				name: "RequestsNb",
+				setsName: ["Excellent", "Medium", "Bad"],
+				sets: [
+					[0,0,0,10],
+					[0,10,10,30],
+					[10,30,40,40]
+				]
+			},
+			{
+				name: "DOMsize",
+				setsName: ["Excellent", "Medium", "Bad"],
+				sets: [
+					[0,0,0,10],
+					[0,10,10,30],
+					[10,30,40,40]
+				]
+			}
+		],
+		variable_output: {
+			name: "Desirability",
+			setsName: ["Undesirable", "Desirable", "Very Desirable"],
+			sets: [
+				[0,0,25,50],
+				[25,50,50,75],
+				[50,75,100,100]
+			]
+		},
+		inferences: [
+			[0,2,0],
+			[0,1,2],
+			[2,1,0]
+		]
+	};
 
-        console.log("Data testé : ",url_data[i]);
-        const fuzzyVal = getFuzzyValue(url_data[i],result);    
-        fuzzification[i] = fuzzyVal;
-    }
+    var f1 = new FuzzyLogic()
 
-    // test 2 avec c2 : Nombres d'etags
-    //const result2 = getSpecificData(data,"etagsNb")
-    //if(result2) getFuzzyValue(45,result2,true);
+    const result = f1.getResult(obj);
+    console.log(result);
 }
-
 
 function getFuzzyValue(value,data,inverse=false) {
     // For simplicity and for now ... The membership function will be triangle.
     /*
     exemple of membership function c1 : size of webpage
-
     excelent     medium   bad
         |\        / \     /|
         | \      /   \   / |
@@ -109,7 +95,6 @@ function getFuzzyValue(value,data,inverse=false) {
         |   \  /       /   |
         |    \/       / \  |
         |___/_\______/___\_|____
-
     */
     let min = data.min;
     let aver = data.average;
@@ -131,3 +116,4 @@ function getFuzzyValue(value,data,inverse=false) {
     return [excellant,medium,bad];
 };
 
+main();
